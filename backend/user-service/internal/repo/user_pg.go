70ee5cb3d8530a57a277daee4bf2e017e1b99710
Vladimir <74735618+qsoulior@users.noreply.github.com>
2023-11-04 21:39:13 +0300

    Implement user repo methods

--- backend/user-service/internal/repo/user_pg.go
+++ backend/user-service/internal/repo/user_pg.go
@@ -17,10 +17,14 @@ func NewUserPG(pg *postgres.Postgres) *UserPG {
 }
 
 func (u *UserPG) Create(ctx context.Context, data entity.User) (*entity.User, error) {
-	const query = `INSERT INTO "data"(email, password, first_name, last_name, patronymic, phone, telegram) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`
+	const query = `INSERT INTO "data"
+			(email, password, first_name, last_name, patronymic, phone, telegram) 
+			VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`
 
 	var user entity.User
-	err := u.Pool.QueryRow(ctx, query, data.Email, data.Password, data.FirstName, data.LastName, data.Patronymic, data.Phone, data.Telegram).Scan(&user.ID, &user.Email, &user.Password, &user.FirstName, &user.LastName, &user.Patronymic, &user.Phone, &user.Telegram)
+	err := u.Pool.
+		QueryRow(ctx, query, data.Email, data.Password, data.FirstName, data.LastName, data.Patronymic, data.Phone, data.Telegram).
+		Scan(&user.ID, &user.Email, &user.Password, &user.FirstName, &user.LastName, &user.Patronymic, &user.Phone, &user.Telegram)
 
 	if err != nil {
 		return nil, err
@@ -28,14 +32,45 @@ func (u *UserPG) Create(ctx context.Context, data entity.User) (*entity.User, er
 
 	return &user, nil
 }
+
 func (u *UserPG) GetByID(ctx context.Context, id uuid.UUID) (*entity.User, error) {
 	const query = `SELECT * FROM "data" WHERE id = $1`
 
 	var user entity.User
-	err := u.Pool.QueryRow(ctx, query, id).Scan(&user.ID, &user.Email, &user.Password, &user.FirstName, &user.LastName, &user.Patronymic, &user.Phone, &user.Telegram)
+	err := u.Pool.
+		QueryRow(ctx, query, id).
+		Scan(&user.ID, &user.Email, &user.Password, &user.FirstName, &user.LastName, &user.Patronymic, &user.Phone, &user.Telegram)
 
 	return &user, err
 }
-func (u *UserPG) GetByName(ctx context.Context, name string) (*entity.User, error)
-func (u *UserPG) UpdatePassword(ctx context.Context, id uuid.UUID, password []byte) error
-func (u *UserPG) DeleteByID(ctx context.Context, id uuid.UUID) error
+
+func (u *UserPG) GetByName(ctx context.Context, name string) (*entity.User, error) {
+	const query = `SELECT * FROM "data" WHERE name = $1`
+
+	var user entity.User
+	err := u.Pool.
+		QueryRow(ctx, query, name).
+		Scan(&user.ID, &user.Email, &user.Password, &user.FirstName, &user.LastName, &user.Patronymic, &user.Phone, &user.Telegram)
+
+	return &user, err
+}
+
+func (u *UserPG) UpdatePassword(ctx context.Context, id uuid.UUID, password []byte) error {
+	const query = `UPDATE "data" SET password = $2 WHERE id = $1`
+
+	if _, err := u.Pool.Exec(ctx, query, id, password); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *UserPG) DeleteByID(ctx context.Context, id uuid.UUID) error {
+	const query = `DELETE FROM "data" WHERE id = $1`
+
+	if _, err := u.Pool.Exec(ctx, query, id); err != nil {
+		return err
+	}
+
+	return nil
+}
