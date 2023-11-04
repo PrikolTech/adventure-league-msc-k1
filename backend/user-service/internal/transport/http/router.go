498d0e4f8148fb2055c0ef13123069819ec2d8b1
Vladimir <74735618+qsoulior@users.noreply.github.com>
2023-11-04 23:26:06 +0300

    Implement user handler

--- backend/user-service/internal/transport/http/router.go
+++ backend/user-service/internal/transport/http/router.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"user-service/internal/service"
+	"user-service/internal/transport/http/handler"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -14,8 +15,15 @@ type Services struct {
 func Router(services Services) *chi.Mux {
 	mux := chi.NewMux()
 
+	user := handler.NewUser(services.User)
+
 	mux.Route("/", func(r chi.Router) {
 		r.Route("/user", func(r chi.Router) {
+			r.Get("/", user.Get)
+			r.Post("/", user.Create)
+			r.Patch("/", user.Update)
+			r.Delete("/", user.Delete)
+			r.Post("/authenticate", user.Authenticate)
 		})
 		r.Route("/role", func(r chi.Router) {
 		})
