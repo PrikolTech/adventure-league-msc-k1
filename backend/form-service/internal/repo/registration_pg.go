c9838b48ebdb7ff92e51b812cad5b6ffa84ea6ab
Vladimir <74735618+qsoulior@users.noreply.github.com>
2023-11-08 12:47:16 +0300

    Implement status update

--- backend/form-service/internal/repo/registration_pg.go
+++ backend/form-service/internal/repo/registration_pg.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"form-service/internal/entity"
 	"form-service/pkg/postgres"
+
+	"github.com/gofrs/uuid/v5"
 )
 
 type RegistrationPG struct {
@@ -19,10 +21,21 @@ func (r *RegistrationPG) Create(ctx context.Context, data entity.Registration) (
 		(email, initiator_first_name, initiator_last_name, initiator_patronymic, birthdate, supervisor_first_name, supervisor_last_name, supervisor_patronymic, department, post, history, achievements, motivation, phone, telegram, status) 
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *`
 
-	var user entity.Registration
+	var registration entity.Registration
 	err := r.Pool.
 		QueryRow(ctx, query, data.Email, data.Initiator.FirstName, data.Initiator.LastName, data.Initiator.Patronymic, data.Birthdate, data.Supervisor.FirstName, data.Supervisor.LastName, data.Supervisor.Patronymic, data.Department, data.Post, data.History, data.Achievements, data.Motivation, data.Phone, data.Telegram, data.Status).
-		Scan(&user.ID, &user.Email, &user.Initiator.FirstName, &user.Initiator.LastName, &user.Initiator.Patronymic, &user.Birthdate, &user.Supervisor.FirstName, &user.Supervisor.LastName, &user.Supervisor.Patronymic, &data.Department, &data.Post, &data.History, &data.Achievements, &data.Motivation, &user.Phone, &user.Telegram, &data.Status)
+		Scan(&registration.ID, &registration.Email, &registration.Initiator.FirstName, &registration.Initiator.LastName, &registration.Initiator.Patronymic, &registration.Birthdate, &registration.Supervisor.FirstName, &registration.Supervisor.LastName, &registration.Supervisor.Patronymic, &registration.Department, &registration.Post, &registration.History, &registration.Achievements, &registration.Motivation, &registration.Phone, &registration.Telegram, &registration.Status)
 
-	return &user, err
+	return &registration, err
+}
+
+func (r *RegistrationPG) UpdateStatus(ctx context.Context, id uuid.UUID, status entity.Status) (*entity.Registration, error) {
+	const query = `UPDATE registration SET status = $2 WHERE id = $1 RETURNING *`
+
+	var registration entity.Registration
+	err := r.Pool.
+		QueryRow(ctx, query, id, status).
+		Scan(&registration.ID, &registration.Email, &registration.Initiator.FirstName, &registration.Initiator.LastName, &registration.Initiator.Patronymic, &registration.Birthdate, &registration.Supervisor.FirstName, &registration.Supervisor.LastName, &registration.Supervisor.Patronymic, &registration.Department, &registration.Post, &registration.History, &registration.Achievements, &registration.Motivation, &registration.Phone, &registration.Telegram, &registration.Status)
+
+	return &registration, err
 }
--- backend/form-service/internal/repo/repo.go
+++ backend/form-service/internal/repo/repo.go
@@ -3,8 +3,11 @@ package repo
 import (
 	"context"
 	"form-service/internal/entity"
+
+	"github.com/gofrs/uuid/v5"
 )
 
 type Registration interface {
 	Create(ctx context.Context, data entity.Registration) (*entity.Registration, error)
+	UpdateStatus(ctx context.Context, id uuid.UUID, status entity.Status) (*entity.Registration, error)
 }
