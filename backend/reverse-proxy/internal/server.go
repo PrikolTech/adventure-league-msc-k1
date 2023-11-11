b8d2e1a65582c2e8e76fb10b5efe9d8fe90aed22
Vladimir <74735618+qsoulior@users.noreply.github.com>
2023-11-11 21:25:49 +0300

    Fix cors

--- backend/reverse-proxy/internal/server.go
+++ backend/reverse-proxy/internal/server.go
@@ -2,8 +2,6 @@ package internal
 
 import (
 	"net/http"
-
-	"github.com/rs/cors"
 )
 
 type ServerOptions struct {
@@ -13,17 +11,17 @@ type ServerOptions struct {
 
 func NewServer(proxy ReverseProxy, opts ServerOptions) *http.Server {
 	mux := http.NewServeMux()
-	c := cors.New(cors.Options{
-		AllowedOrigins:   opts.Origins,
-		AllowedMethods:   []string{http.MethodHead, http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete},
-		AllowedHeaders:   []string{"*"},
-		AllowCredentials: true,
-	})
+	// c := cors.New(cors.Options{
+	// 	AllowedOrigins:   opts.Origins,
+	// 	AllowedMethods:   []string{http.MethodHead, http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete},
+	// 	AllowedHeaders:   []string{"*"},
+	// 	AllowCredentials: true,
+	// })
 
 	mux.Handle("/", proxy.Handler())
 
 	return &http.Server{
 		Addr:    opts.Addr,
-		Handler: c.Handler(mux),
+		Handler: mux,
 	}
 }
